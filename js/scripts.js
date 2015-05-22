9de247181d6825b4e3157b5d936f9329fdb3cfb9
Ashley Jhaveri <ashleyjhaveri@gmail.com>
2015-05-22 11:41:33 -0700

    dial works

--- js/scripts.js
+++ js/scripts.js
@@ -10,29 +10,65 @@ $('#static').show();
 
 $('#channel').click( 
 	function () {
+		$('#dial').removeClass('dial' + click);
 		click++;
 		player[1].play;
-		$('#dial').addClass('dial' + click)
+		$('#dial').addClass('dial' + click);
 		
-		console.log(player);
 
 			if (click > 3) {
 				$('#static').hide();
 				$('#trailer').show();
 				player[0].play();
 				click = 0;
+				$('#dial').removeClass('dial4');
 			}
+
+			console.log('dial' + click);
 	}
 );
 
-
+$('#channel-1').click(
+	function() {
+		$('#dial').removeClass('dial' + click);
+		click = 1;
+		$('#dial').addClass('dial' + click);
+		$('#static').show();
+		$('#trailer').hide();
+		player[1].play();
+	}
+);
+$('#channel-2').click(
+	function() {
+		$('#dial').removeClass('dial' + click);
+		click = 2;
+		$('#dial').addClass('dial' + click);
+		$('#static').show();
+		$('#trailer').hide();
+		player[1].play();
+	}
+);
+$('#channel-3').click(
+	function() {
+		$('#dial').removeClass('dial' + click);
+		click = 3;
+		$('#dial').addClass('dial' + click);
+		$('#static').show();
+		$('#trailer').hide();
+		player[1].play();
+	}
+);
 
 $('#channel-4').click(
 	function() {
+		$('#dial').removeClass('dial' + click);
+		click = 0;
 		$('#static').hide();
 		$('#trailer').show();
 		player[0].play();
+
 	}
+
 
 );
 
